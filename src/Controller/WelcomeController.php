27774e08a69d9a02b61cc7b4b204be66b90942aa
Michael Sommer <michael.sommer@rewe-digital.com>
2023-01-08 18:38:17 +0100

    add welcome pages

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WelcomeController extends AbstractController
+{
+    #[Route('/')]
+    public function indexNoLocale(): Response
+    {
+        return $this->redirectToRoute('welcome', ['_locale' => 'de']);
+    }
+
+    #[Route('/{_locale<%app.supported_locales%>}')]
+    public function index(): Response
+    {
+        return $this->redirectToRoute('welcome');
+    }
+
+    #[Route('/{_locale<%app.supported_locales%>}/welcome', name: 'welcome')]
+    public function welcome(): Response
+    {
+        if ($this->getParameter('app.is_active')) {
+            return $this->activeWelcome();
+
+        } else {
+            return $this->render('welcome/waiting.html.twig');
+        }
+    }
+
+    public function activeWelcome(): Response
+    {
+        if ($this->isGranted('ROLE_USER')) {
+            return $this->activeAuthenticatedWelcome();
+        } else {
+            return $this->activeAnonymousWelcome();
+        }
+    }
+
+    public function activeAuthenticatedWelcome(): Response
+    {
+        return $this->render('welcome/index.html.twig');
+    }
+
+    public function activeAnonymousWelcome(): Response
+    {
+        return $this->render('welcome/anonymous.html.twig');
+    }
+}
