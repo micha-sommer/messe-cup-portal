21390fee8b91f3c88992d6714f18af534f3c8cb2
Michael Sommer <michael.sommer@rewe-digital.com>
2023-02-07 08:56:48 +0100

    add invoice generation
    First, it is only shown in admin overview. Later, we also want to send it out to participants by mail.

---
+++ src/Controller/InvoiceController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Registration;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use TCPDF;
+
+#[Route('/{_locale<%app.supported_locales%>}/invoice')]
+class InvoiceController extends AbstractController
+{
+    #[Route('/{id}', name: 'invoice_show', methods: "GET")]
+    public function show(Registration $registration): Response
+    {
+        if ($this->getUser() !== $registration) {
+            $this->denyAccessUnlessGranted("ROLE_ADMIN");
+        }
+
+        $pdf = $this->getInvoicePDF($registration);
+
+        $pdf->Output('Rechnung.pdf');
+
+        return new Response(200);
+    }
+
+    private function getInvoicePDF(Registration $registration): TCPDF
+    {
+        $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'utf-8', false);
+        $pdf->setCreator(PDF_CREATOR);
+        $pdf->setAuthor('Erfurter Judo-Club e.V.');
+        $pdf->setTitle('Rechnung Messe-Cup/EGA-Pokal');
+        /** @noinspection NullPointerExceptionInspection */
+        $pdf->setSubject('Rechnung für ' . $registration->getClub());
+
+        // remove default header/footer
+        $pdf->setPrintHeader(false);
+        $pdf->setPrintFooter(false);
+
+        // Auswahl des Font
+        $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+        // Auswahl der MArgins
+        $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+        // Automatisches Autobreak der Seiten
+        $pdf->SetAutoPageBreak(true, PDF_MARGIN_BOTTOM);
+        // Image Scale
+        $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+        // Schriftart
+        $pdf->SetFont('dejavusans', '', 10);
+        // Neue Seite
+        $pdf->AddPage();
+
+        $html = $this->render('invoice/pdf.html.twig', ['registration' => $registration])->getContent();
+        $pdf->writeHTML($html, true, false, true);
+        return $pdf;
+    }
+}
